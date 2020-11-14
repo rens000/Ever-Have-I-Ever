5a535f1b8367f5d55c0e0c98c7a0a2a7e441ae93
Aria Rens <ariarens@Arias-MacBook-Pro.local>
2020-11-14 16:31:06 -0500

    third commit

--- boston hacks 2.py
+++ boston hacks 2.py
@@ -49,16 +49,84 @@ def get_num_players():
         pygame.display.flip()
     return num
 
-def get_names():
+
+
+    text = font.render('How many players do you have?', True, blue, white) 
+    textRect = text.get_rect()  
+    textRect.center = (x // 2, y // 2.5)
+    screen.fill(white) 
+    screen.blit(text, textRect)
+    pygame.display.update()
     
-    screen.fill(white)
+
+
+def display_name_question(player_number):
+    text = font.render('Player ' + str(player_number) + ': What is your name?', True, blue, white)
+    textRect = text.get_rect()  
+    textRect.center = (x // 2, y // 2.5)
+    screen.fill(white) 
+    screen.blit(text, textRect)
     pygame.display.update()
 
 
-    
-    
+def get_names_question(player_number):
+    display_name_question(player_number)
+    num = ''
+    check = True
+    while check:
+        for evt in pygame.event.get():
+            if evt.type == KEYDOWN:
+                screen.fill((250,250,250))
+                display_name_question(player_number)
+                if evt.key == K_BACKSPACE:
+                    num = num[:-1]
+                elif evt.key == K_RETURN:
+                    check = False
+                else:
+                    num += evt.unicode
+            elif evt.type == QUIT:
+                return
+        block = font.render(num, True, (blue))
+        rect = block.get_rect()
+        rect.center = screen.get_rect().center
+        screen.blit(block, rect)
+        pygame.display.flip()
+    return num
 
-get_num_players()
+    text = font.render("What is your name?", True, blue, white) 
+    textRect = text.get_rect()  
+    textRect.center = (x // 2, y // 2.5)
+    screen.fill(white) 
+    screen.blit(text, textRect)
+    pygame.display.update()
+
+    
+def get_names():
+    player_number = 1
+    list_of_names= []
+    num_players = int(get_num_players())
+    screen.fill(white)
+    pygame.display.update()
+    for i in range(num_players):
+        
+        name = get_names_question(player_number)
+        list_of_names.append(name)
+        player_number += 1
+        
+    return list_of_names
+        
+def names_display():
+    screen.fill(white)
+    pygame.display.update()
+    names = get_names()
+    
+        
+
+
+print(get_names())
+names_display()
+    
+    
 
 
 
